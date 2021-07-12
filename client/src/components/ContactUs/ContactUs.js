5b5c13f67fac089be5d7231c1f3e74f4d1b8b9a6
Armin <arminch1997@gmail.com>
2021-07-12 14:42:07 -0700

    contact page

---
+++ client/src/components/ContactUs/ContactUs.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import emailjs from 'emailjs-com'
+import { Button, Form, FormGroup, Label, Input, FormText, Container } from 'reactstrap'
+
+export default function ContactUs() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_nsz1vwr', 'contact_form', e.target, 'user_2P86bgZaM1zTmZW9Ad2qM')
+      .then((result) => {
+        console.log(result.text)
+        alert('Your message has been submitted. I will get back to you shortly!')
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <FormGroup>
+        <Label for="name">Name</Label>
+        <Input type="text" name="user_name" placeholder="name" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="email">Email</Label>
+        <Input type="email" name="user_email" placeholder="email" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="message">Your Message</Label>
+        <Input type="textarea" name="message" />
+      </FormGroup>
+      <input type="submit" value="Send" />
+    </form>
+  );
+}
--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,6 +1,24 @@
+import { Button, Form, FormGroup, Label, Input, FormText, Container } from 'reactstrap'
+import ContactUs from '../../components/ContactUs'
 const Contact = () => {
   return (
-    <h1>This is the Contact page.</h1>
+    <Container className="mt-3">
+      {/* <FormGroup>
+        <Label for="name">Name</Label>
+        <Input type="name" name="name" placeholder="name" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="email">Email</Label>
+        <Input type="email" name="email" placeholder="email" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="message">Your Message</Label>
+        <Input type="textarea" name="text" id="exampleText" />
+      </FormGroup>
+      <Button color="primary">Submit Message</Button> */}
+      <ContactUs />
+    </Container>
+    
   )
 }
 
